5861f0fd69adf7cb181420a84451514e5008373d
Maurizio Tomasi <ziotom78@gmail.com>
2022-01-11 11:27:28 +0100

    Improve the way ROOT is installed and remove the tarball

--- install_root.sh
+++ install_root.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-readonly filename="root_v6.24.06.Linux-ubuntu18-x86_64-gcc7.5.tar.gz"
+readonly filename="root_v6.25.01.Linux-ubuntu18-x86_64-gcc7.5.tar.gz"
 
 wget https://root.cern/download/${filename} && \
-	tar -zxvf ${filename}
+	tar -zxvf ${filename} && rm -f ${filename}
 
-source setup.sh
+cat <<EOF
+To run "root-config", you must first run
+
+    source setup.sh
+EOF
